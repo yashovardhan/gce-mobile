53052046e57716d866067baf1911fbfeba4ff74a
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-03-03 03:01:42 +0530

    ExpertScreen.js: Add list of experts

--- screens/ExpertScreen.js
+++ screens/ExpertScreen.js
@@ -1,17 +1,48 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView, View, Platform } from 'react-native';
+import { Icon, List, ListItem, Header } from 'react-native-elements';
+
+const experts = require('../data/experts.json');
 
 class ExpertScreen extends Component {
+  static navigationOptions = {
+    title: 'Experts List',
+    tabBarIcon: ({ tintColor }) => {
+      return <Icon name="view-list" size={30} color={tintColor} />;
+    },
+  };
+
   render() {
     return (
-      <View>
-        <Text>ExpertScreen</Text>
-        <Text>ExpertScreen</Text>
-        <Text>ExpertScreen</Text>
-        <Text>ExpertScreen</Text>
-        <Text>ExpertScreen</Text>
-        <Text>ExpertScreen</Text>
-        <Text>ExpertScreen</Text>
+      <View style={{ flex: 1 }}>
+        <Header
+          statusBarProps={{ barStyle: 'light-content' }}
+          centerComponent={{
+            text: 'GitHub Campus Experts',
+            style: {
+              color: '#fff',
+              fontSize: 18,
+            },
+          }}
+          outerContainerStyles={{
+            backgroundColor: '#D71E3C',
+            height: Platform.OS === 'ios' ? 70 : 70 + 10,
+          }}
+          innerContainerStyles={{ justifyContent: 'space-around' }}
+        />
+        <ScrollView>
+          <List>
+            {experts.map(expert => (
+              <ListItem
+                roundAvatar
+                avatar={{ uri: expert.avatar }}
+                key={expert.github}
+                title={expert.name ? expert.name : expert.shortname}
+                subtitle={expert.university}
+              />
+            ))}
+          </List>
+        </ScrollView>
       </View>
     );
   }
