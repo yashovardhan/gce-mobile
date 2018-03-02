96ca3e2d058d9f54e68ce4587b9e36cd992386b6
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-03-02 20:53:51 +0530

    screens: Add MapScreen.js & ExpertScreen.js

---
+++ screens/ExpertScreen.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class ExpertScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>ExpertScreen</Text>
+        <Text>ExpertScreen</Text>
+        <Text>ExpertScreen</Text>
+        <Text>ExpertScreen</Text>
+        <Text>ExpertScreen</Text>
+        <Text>ExpertScreen</Text>
+        <Text>ExpertScreen</Text>
+      </View>
+    );
+  }
+}
+
+export default ExpertScreen;
