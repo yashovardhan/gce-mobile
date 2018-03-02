2687f1b5857edd0a1cef83fa9153ebe1aac75326
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-03-02 20:54:16 +0530

    App.js: Intigrate screens in TabNavigator

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { TabNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
+import MapScreen from './screens/MapScreen';
+import ExpertScreen from './screens/ExpertScreen';
+
+class App extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+    const MainNavigator = TabNavigator(
+      {
+        map: { screen: MapScreen },
+        expert: { screen: ExpertScreen },
+      },
+      {
+        tabBarPosition: 'bottom',
+        swipeEnabled: false,
+        tabBarOptions: {
+          labelStyle: { fontSize: 10 },
+        },
+      }
     );
+    return <MainNavigator />;
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
