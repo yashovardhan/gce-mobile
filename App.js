bce39507f88c2256f4d613e2cbb66c2ba7d0744e
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-03-03 17:02:23 +0530

    App.js: Integrate screens into navigators

--- App.js
+++ App.js
@@ -1,35 +1,68 @@
 import React, { Component } from 'react';
-import { TabNavigator } from 'react-navigation';
+import { StackNavigator, TabNavigator } from 'react-navigation';
 
+import HomeScreen from './screens/HomeScreen';
 import MapScreen from './screens/MapScreen';
+import BAEScreen from './screens/BAEScreen';
+import ProfileScreen from './screens/ProfileScreen';
 import ExpertScreen from './screens/ExpertScreen';
 
 class App extends Component {
   render() {
-    const MainNavigator = TabNavigator(
+    const MainNavigator = StackNavigator(
       {
-        map: { screen: MapScreen },
-        expert: { screen: ExpertScreen },
+        join: { screen: BAEScreen },
+        home: { screen: HomeScreen },
+        find: {
+          screen: TabNavigator(
+            {
+              map: { screen: MapScreen },
+              expert: {
+                screen: StackNavigator(
+                  {
+                    list: { screen: ExpertScreen },
+                    profile: { screen: ProfileScreen },
+                  },
+                  {
+                    headerMode: 'none',
+                    lazy: true,
+                  }
+                ),
+              },
+            },
+            {
+              tabBarPosition: 'bottom',
+              swipeEnabled: true,
+              lazy: true,
+              animationEnabled: true,
+              initialRouteName: 'expert',
+              tabBarOptions: {
+                showLabel: false,
+                showIcon: true,
+                upperCaseLabel: false,
+                labelStyle: { fontSize: 12 },
+                scrollEnabled: false,
+                activeTintColor: '#D71E3C',
+                inactiveTintColor: 'gray',
+                style: {
+                  backgroundColor: 'white',
+                },
+              },
+            }
+          ),
+        },
       },
       {
-        tabBarPosition: 'bottom',
-        swipeEnabled: false,
-        lazy: true,
-        initialRouteName: 'expert',
-        tabBarOptions: {
-          showLabel: false,
-          showIcon: true,
-          upperCaseLabel: false,
-          labelStyle: { fontSize: 12 },
-          scrollEnabled: false,
-          activeTintColor: '#D71E3C',
-          inactiveTintColor: 'gray',
-          style: {
-            backgroundColor: 'white',
-          },
+        navigationOptions: {
+          tabBarVisible: false,
         },
+        initialRouteName: 'home',
+        animationEnabled: true,
+        headerMode: 'none',
+        lazy: true,
       }
     );
+
     return <MainNavigator />;
   }
 }
