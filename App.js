9d7f3fa712026343b6a33097de5f5247e63373f1
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-03-04 01:15:48 +0530

    App.js: Performance Fixes

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ class App extends Component {
                   },
                   {
                     headerMode: 'none',
-                    lazy: true,
+                    lazy: false,
                   }
                 ),
               },
@@ -33,8 +33,8 @@ class App extends Component {
             {
               tabBarPosition: 'bottom',
               swipeEnabled: true,
-              lazy: true,
-              animationEnabled: true,
+              lazy: false,
+              animationEnabled: false,
               initialRouteName: 'expert',
               tabBarOptions: {
                 showLabel: false,
@@ -53,13 +53,10 @@ class App extends Component {
         },
       },
       {
-        navigationOptions: {
-          tabBarVisible: false,
-        },
-        initialRouteName: 'home',
-        animationEnabled: true,
         headerMode: 'none',
-        lazy: true,
+        initialRouteName: 'home',
+        lazy: false,
+        animationEnabled: true,
       }
     );
 
