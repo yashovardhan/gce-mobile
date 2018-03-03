e1dbe7ae424ddbc78c15acdded34240b012f232a
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-03-03 16:43:31 +0530

    screens: Add ProfileScreen.js

---
+++ screens/ProfileScreen.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class ProfileScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>ProfileScreen</Text>
+        <Text>ProfileScreen</Text>
+        <Text>ProfileScreen</Text>
+        <Text>ProfileScreen</Text>
+        <Text>ProfileScreen</Text>
+        <Text>ProfileScreen</Text>
+      </View>
+    );
+  }
+}
+
+export default ProfileScreen;
